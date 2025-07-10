48f458d62c81cb934d94608e923dd765a49f3481
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-07-10 13:41:20 +0200

    update endboss-function

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -1,8 +1,8 @@
 class Endboss extends MovableObject {
     constructor() {
         super();
-        this.x = 1000;              // Startposition des Endbosses
-        this.y = 100;               // Startposition des Endbosses
+        this.x = 2400;              // Startposition des Endbosses
+        this.y = 200;               // Startposition des Endbosses
         this.width = 200;           // Breite des Endbosses
         this.height = 200;          // Höhe des Endbosses
         this.speed = 0.5;           // Geschwindigkeit des Endbosses
@@ -16,5 +16,35 @@ class Endboss extends MovableObject {
         ];
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_DEAD);
+        this.loadImage(this.IMAGES_WALKING[0]); 
+        this.animate();             // << NEU: Starte die Animations-Loop
+    }
+
+    /**
+     * Startet ein Intervall, um die Animation des Endbosses zu steuern.
+     * @memberof Endboss
+     */
+    animate() {
+        setInterval(() => {
+            // HIER KÖNNTE LOGIK FÜR VERSCHIEDENE ZUSTÄNDE STEHEN
+            // (z.B. Laufen, Angreifen, Sterben)
+
+            // Einfache Lauf-Animation
+            this.playAnimation(this.IMAGES_WALKING);
+
+        }, 200); // Wechselt das Bild alle 200ms
+    }
+
+    /**
+     * Spielt eine Animation ab, indem sie durch ein Array von Bildpfaden iteriert.
+     * @param {string[]} images - Das Array der Bilder, die animiert werden sollen.
+     * @memberof MovableObject
+     */
+    playAnimation(images) {
+        // Modulo (%) sorgt dafür, dass der Index immer im gültigen Bereich des Arrays bleibt
+        let i = this.currentImage % images.length;
+        let path = images[i];
+        this.img = this.imageCache[path]; // Setzt das zu zeichnende Bild aus dem Cache
+        this.currentImage++;
     }
 }
