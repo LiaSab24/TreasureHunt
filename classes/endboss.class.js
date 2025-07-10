11680f6a9f5367a6ad7e04bc4db263bd8e6c59eb
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-07-10 13:13:55 +0200

    update endboss

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -6,20 +6,13 @@ class Endboss extends MovableObject {
         this.width = 200;           // Breite des Endbosses
         this.height = 200;          // Höhe des Endbosses
         this.speed = 0.5;           // Geschwindigkeit des Endbosses
-        this.health = 1;           // Lebenspunkte des Endbosses
+        this.health = 1;            // Lebenspunkte des Endbosses
         this.IMAGES_WALKING = [
-            'images/enemies/endboss/cobra-snake.png' //,
- //           'images/endboss/Walk/WalkEndboss2.png',
- //           'images/endboss/Walk/WalkEndboss3.png',
- //           'images/endboss/Walk/WalkEndboss4.png',
- //           'images/endboss/Walk/WalkEndboss5.png'
+            'images/enemies/endboss/cobra-snake.png',
+            'images/enemies/endboss/cobra-snake.png'
         ];
         this.IMAGES_DEAD = [
-            'images/enemies/endboss/cobra-snake.png',
-            'images/endboss/Faint/2.png',
-            'images/endboss/Faint/3.png',
-            'images/endboss/Faint/4.png',
-            'images/endboss/Faint/5.png'
+            'images/enemies/endboss/cobra-snake.png'
         ];
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_DEAD);
