8745a3eb3fa0283a583a314b0e2d4d64c4f84d4f
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-07-12 12:36:59 +0200

    update

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -2,14 +2,14 @@ class Endboss extends MovableObject {
     constructor() {
         super();
         this.x = 2400;              // Startposition des Endbosses
-        this.y = 280;               // Startposition des Endbosses
+        this.y = 200;               // Startposition des Endbosses
         this.width = 200;           // Breite des Endbosses
         this.height = 200;          // Höhe des Endbosses
         this.speed = 0.5;           // Geschwindigkeit des Endbosses
         this.health = 3;            // Lebenspunkte des Endbosses
         this.isActuallyDead = false; // Status, ob der Endboss tot ist
         this.IMAGES_WALKING = [
-            'images/enemies/endboss/cobra-snake-move1.png',
+            'images/enemies/endboss/cobra-snake.png',
             'images/enemies/endboss/cobra-snake.png'
         ];
         this.IMAGES_DEAD = [
