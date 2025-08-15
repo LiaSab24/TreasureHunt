b52321bd47df00a7bf26a3d326e35f1a1d50876c
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-08-15 08:52:24 +0200

    added new enemy classes

---
+++ classes/enemy2.class.js
@@ -0,0 +1,24 @@
+class Enemy2 extends Enemy {
+    height = 170;
+    width = 80;
+
+    IMAGES_WALKING = [
+        'images/enemies/enemy/enemy2/Walk2/walk1.png',
+        'images/enemies/enemy/enemy2/Walk2/walk2.png',
+        'images/enemies/enemy/enemy2/Walk2/walk3.png',
+        'images/enemies/enemy/enemy2/Walk2/walk4.png',
+        'images/enemies/enemy/enemy2/Walk2/walk5.png',
+        'images/enemies/enemy/enemy2/Walk2/walk6.png'
+    ];
+
+    IMAGES_DEAD = [
+        'images/enemies/enemy/enemy2/Faint2/faint1.png',
+        'images/enemies/enemy/enemy2/Faint2/faint2.png',
+        'images/enemies/enemy/enemy2/Faint2/faint3.png',
+        'images/enemies/enemy/enemy2/Faint2/faint4.png'
+    ];
+
+    constructor(startX, world) {
+        super(startX, world);
+    }
+}
