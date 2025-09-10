2d8c05f5d02f2fe7ed744d92c82a26c4a00a6801
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-09-10 14:20:26 +0200

    clearing

--- classes/collisions.class.js
+++ classes/collisions.class.js
@@ -1,12 +1,16 @@
 class Collisions {
   world;
 
+    /**
+     * Creates a new Collisions handler for the game world.
+     * @param {World} world - The reference to the game world.
+     */
   constructor(world) {
       this.world = world;
   }
 
     /**
-     * Hauptmethode, die alle Kollisionsprüfungen durchführt.
+     * Main method that performs all collision checks  
      */
     checkAllCollisions() {
         this.checkCoinCollisions();
@@ -17,6 +21,10 @@ class Collisions {
         this.cleanupDestroyedObjects();
     }
 
+    /**
+     * Checks for collisions between the character and coins.
+     * If a collision occurs, the coin is collected and removed.
+     */
     checkCoinCollisions() {
         this.world.coins.forEach((coin, index) => {
             if (this.world.character.isColliding(coin)) {
@@ -28,6 +36,10 @@ class Collisions {
         });
     }
 
+    /**
+     * Checks for collisions between the character and stones.
+     * If a collision occurs, the stone is collected and removed.
+     */
     checkStoneCollisions() {
         this.world.stones.forEach((stone, index) => {
             if (this.world.character.isColliding(stone)) {
@@ -38,14 +50,17 @@ class Collisions {
         });
     }
 
+    /**
+     * Checks for collisions between the character and enemies.
+     * Handles jump on endboss and normal enemy hit.
+     */
     checkEnemyCollisions() {
         this.world.enemies.forEach((enemy) => {
             if (this.world.character.isColliding(enemy) && !enemy.isDead()) {
-                // Sprung auf Endboss
                 if (enemy instanceof Endboss && this.world.character.isAboveGround() && this.world.character.speedY < 0) {
                     enemy.hit();
                     this.world.character.bounce();
-                } // Normaler Treffer durch Gegner
+                } 
                 else if (!this.world.character.isHurt()) {
                     this.world.character.hit();
                     this.world.updateStatusBars();
@@ -54,6 +69,10 @@ class Collisions {
         });
     }
 
+    /**
+     * Checks for collisions between throwable objects (stones) and enemies.
+     * If a collision occurs, the enemy is hit and the stone is destroyed.
+     */
     checkThrowableObjectCollisions() {
         this.world.throwableObjects.forEach((stone) => {
             this.world.enemies.forEach((enemy) => {
@@ -65,17 +84,25 @@ class Collisions {
         });
     }
 
+    /**
+     * Checks for collision between the character and the treasure chest.
+     * The endboss must be defeated before the chest can be opened and the game won.
+     */
     checkTreasureChestCollision() {
         const chest = this.world.treasureChest;
         if (chest && this.world.character.isColliding(chest)) {
             if (this.world.endboss && !this.world.endboss.isDead()) {
-                return; // Endboss muss besiegt sein
+                return; 
             } else {
                 this.world.handleWin();
             }
         }
     }
 
+    /**
+     * Removes destroyed throwable objects and dead enemies from the world.
+     * The endboss remains in the world even if dead.
+     */
     cleanupDestroyedObjects() {
         this.world.throwableObjects = this.world.throwableObjects.filter(stone => !stone.isDestroyed);
         this.world.enemies = this.world.enemies.filter(enemy => !enemy.isDead() || enemy instanceof Endboss);
